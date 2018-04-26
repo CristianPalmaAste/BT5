50d7f71f4b6d458ce2fc4ecaff1bea698d78730a
Cristian Palma <gerardo.palma.aste@gmail.com>
2018-04-25 21:59:38 -0300

    cpalma

--- 01500_funciones_almacenadas/02500_f_profundidad_unte.sql
+++ 01500_funciones_almacenadas/02500_f_profundidad_unte.sql
@@ -1,8 +1,17 @@
-create or replace function f_profundidad_unte( int ) returns int as
+create or replace function f_profundidad_unte( numeric ) returns numeric as
 $$
 declare
-  vIdUnte int;
+  vIdUnte numeric;
+  aux     numeric;
 begin
+  select count(*)
+  into   aux
+  from   unidades_territoriales
+  where  id = $1
+  ;
+  if aux = 0 then
+    return 0;
+  end if;
   select iduntepadre
   into   vIdUnte
   from   unidades_territoriales
@@ -20,6 +29,23 @@ select f_profundidad_unte (1);
 select f_profundidad_unte (20);
 select f_profundidad_unte (100);
 select f_profundidad_unte (1000);
+
+select id, idpais, iduntepadre, nombre, orden, f_profundidad_unte(id) profundidad
+from   unte
+where  f_profundidad_unte(id) = 1
+;
+
+select id, idpais, iduntepadre, nombre, orden, f_profundidad_unte(id) profundidad
+from   unte
+where  nombre                 like '%CA%'
+and    f_profundidad_unte(id) = 2
+;
+
+select id, idpais, iduntepadre, nombre, orden, f_profundidad_unte(id) profundidad
+from   unte
+where  nombre                 like '%MU%'
+and    f_profundidad_unte(id) = 3
+;
 
 \q
 

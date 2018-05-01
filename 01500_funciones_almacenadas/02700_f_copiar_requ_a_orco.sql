ff5ff7f6ba724fd6d0db433a94d29871f3f920b1
Cristian Palma <gerardo.palma.aste@gmail.com>
2018-05-01 14:46:12 -0300

    cpalma

---
+++ 01500_funciones_almacenadas/02700_f_copiar_requ_a_orco.sql
@@ -0,0 +1,107 @@
+create or replace function f_copiar_requ_a_orco() returns trigger as
+$body$
+declare
+  Vidorco                   numeric(20,0);
+  Vcorrelativo              numeric(20,0);
+  Vid                       numeric(20,0);
+  Vidrequ                   numeric(20,0);
+  Vcorrelativo              numeric(20,0);
+  Vidprod                   numeric(20,0);
+  Vidunmp                   numeric(20,0);
+  Vidserv                   numeric(20,0);
+  Vidunms                   numeric(20,0);
+  Votroinsumo               varchar(1000);
+  Vcantidad                 numeric(20,0);
+  Vcantidad                 numeric(20,0);
+  Viddeoc                   numeric(20,0);
+  C_dere cursor for
+    select id
+          ,idrequ
+          ,correlativo
+          ,idprod
+          ,idunmp
+          ,idserv
+          ,idunms
+          ,otroinsumo
+          ,cantidad
+    from   detalles_requisiciones
+    where  idrequ = new.id
+    ;
+begin
+  if old.idereq = 5 and new.idereq = 7 then
+    select nextval('orco_seq')
+    into   Vidorco
+    ;
+    select max(correlativo)
+    into   Vcorrelativo
+    from   ordenes_compras
+    where  idempr = new.idempr
+    ;
+    if Vcorrelativo is null then
+      Vcorrelativo := 1;
+    else
+      Vcorrelativo := Vcorrelativo + 1;
+    end if;
+    insert into ordenes_compras (id                       -- numeric(20,0)   not null
+                                ,idempr                   -- numeric(20,0)   not null
+                                ,correlativo              -- numeric(20,0)   not null
+                                ,idrequ                   -- numeric(20,0)       null
+                                ,idgere                   -- numeric(20,0)       null
+                                ,idproy                   -- numeric(20,0)       null
+                                ,idline                   -- numeric(20,0)       null
+                                ,idceco                   -- numeric(20,0)       null
+                                ,idtare                   -- numeric(20,0)       null
+                                ,idesoc                   -- numeric(20,0)       null
+                                ,idusuacrearegistro       -- numeric(20,0)   not null
+                                ,fechacrearegistro        -- timestamp       not null
+                                ,idusuamodifregistro      -- numeric(20,0)       null
+                                ,fechamodifregistro       -- timestamp           null
+                                ,idusuaborraregistro      -- numeric(20,0)       null
+                                ,fechaborraregistro       -- timestamp           null
+                                )
+    values (Vidorco                                 -- id                       numeric(20,0)   not null
+           ,new.idempr                              -- idempr                   numeric(20,0)   not null
+           ,Vcorrelativo                            -- correlativo              numeric(20,0)   not null
+           ,new.id                                  -- idrequ                   numeric(20,0)       null
+           ,new.idgere                              -- idgere                   numeric(20,0)       null
+           ,new.idproy                              -- idproy                   numeric(20,0)       null
+           ,new.idline                              -- idline                   numeric(20,0)       null
+           ,new.idceco                              -- idceco                   numeric(20,0)       null
+           ,new.idtare                              -- idtare                   numeric(20,0)       null
+           ,1                                       -- idesoc                   numeric(20,0)       null
+           ,new.idusuacrearegistro                  -- idusuacrearegistro       numeric(20,0)   not null
+           ,current_timestamp                       -- fechacrearegistro        timestamp       not null
+           ,null                                    -- idusuamodifregistro      numeric(20,0)       null
+           ,null                                    -- fechamodifregistro       timestamp           null
+           ,null                                    -- idusuaborraregistro      numeric(20,0)       null
+           ,null                                    -- fechaborraregistro       timestamp           null
+           )
+    ;
+    open C_dere;
+    loop
+      fetch C_dere into Vid
+                       ,Vidrequ
+                       ,Vcorrelativo
+                       ,Vidprod
+                       ,Vidunmp
+                       ,Vidserv
+                       ,Vidunms
+                       ,Votroinsumo
+                       ,Vcantidad
+                       ;
+      exit when not found;
+      select nextval('deoc_seq')
+      into   Viddeoc
+      ;
+
+
+
+    end loop;
+    close C_dere;
+  end if;
+  return new;
+end;
+$body$ LANGUAGE plpgsql;
+
+\q
+
